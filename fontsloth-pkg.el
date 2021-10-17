bd6362a6435ed2f4008f87cf596fcfb3c967f103
Jo Gay <jo.gay@mailfence.com>
2021-10-17 01:27:22 -0700

    define the emacs package for fontsloth

---
+++ fontsloth-pkg.el
@@ -0,0 +1,17 @@
+(define-package "fontsloth" "0.0.1"
+  "the slowest font renderer in the world, written in pure elisp"
+  '((cl-lib "0.5")
+    (emacs "26.1")
+    (logito "0.1")
+    (pcache "0.5"))
+    :keywords
+    '("font" "glyph" "ttf" "otf" "parsing" "rasterization")
+    :authors
+    '(("jo" . "jo.gay@mailfence.com"))
+    :maintainer
+    '("jo" . "jo.gay@mailfence.com"))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;;; fontsloth-pkg.el ends here
