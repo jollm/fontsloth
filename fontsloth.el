8ce1802b356962296a492d90cc9ae62e06c7ae43
Jo Gay <jo.gay@mailfence.com>
2021-10-30 22:08:12 -0700

    Update package usage instructions to point to README.org

--- fontsloth.el
+++ fontsloth.el
@@ -29,8 +29,13 @@
 ;; inspired by fontdue, the fastest font renderer in the world, written in pure
 ;; rust
 
+;; *Please see the website for a detailed README.*
+
 ;; To use this module, load and enable it as follows:
 ;;   (use-package fontsloth)
+;;
+;; If you also want layout functions (includes fontsloth):
+;;   (use-package fontsloth-layout)
 ;;
 
 ;;; Code:
